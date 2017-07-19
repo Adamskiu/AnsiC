6b4f4d571fd7eb455c18586a50c5dd061cc4ef2a
adamskiu <awroblak@wi.zut.edu.pl>
2017-07-19 15:56:36 +0200

    Pierwszy napisany program - Konwersja stopni Fahrenheita na Celcjusza.

--- fahr2Celsius/main.c
+++ fahr2Celsius/main.c
@@ -2,5 +2,17 @@
 
 main()
 {
+    float fahr, celsius;
+    int lower, upper, step;
 
+    lower = 0;      // Dolne ograniczenie skali temperatury.
+    upper = 300;    // Górne ograniczenie.
+    step = 20;      // Krok.
+
+    fahr = lower;
+    while (fahr <= upper){
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
 }

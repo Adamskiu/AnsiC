60a12dccaade3a1531d84fd297da3d7f9efee7db
adamskiu <awroblak@wi.zut.edu.pl>
2017-07-22 18:03:45 +0200

    Rozdział 1.7 - Ułomna funkcja potęgująca.

---
+++ funkcjaPower/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power(int , int );
+
+// Test funkcji power
+int main()
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+        printf("%d %d %d\n", i, power(i,2), power(i,3));
+    return 0;
+}
+
+// power: podnosi podstawę do potęgi n; n >= 0
+int power(int base, int n)
+{
+    int i, p;
+
+    p = 1;
+    for (i =1; i <= n; ++i)
+        p = p * base;
+    return p;
+}
